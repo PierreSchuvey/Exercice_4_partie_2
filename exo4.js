e1e46813e7616301b29077a431092bfd129d7a4a
Pierre Schuvey <pierre.schuveye2n@gmail.com>
2017-10-25 09:27:26 +0200

    Fin de l'exercice 4 de la partie 2 du JS

---
+++ exo4.js
@@ -0,0 +1,6 @@
+function deleteAll()
+{
+  document.getElementById("nom").value="";
+  var firstname = document.getElementById("prenom").value="";
+  var city = document.getElementById("ville").value="";
+}
